0d6e8941c058de1ca21a7a6ce9f5223c8247463a
Gersi Doko <gersi.doko@unh.edu>
2024-02-29 20:07:04 -0500

    NixOS Rebuild: 93 current  2024-02-29 20:05:18  23.11.4575.526d051b128b  6.1.78                          *

--- rebuild_switch.sh
+++ rebuild_switch.sh
@@ -12,5 +12,5 @@ echo "Pushing to git..."
 GEN=$(nixos-rebuild --fast list-generations | grep current)
 echo "Generation: $GEN"
 sudo git commit -am "NixOS Rebuild: $GEN"
-git push -u origin main
+sudo git push -u origin main
 popd
