98f956deda68f6ce016d4043f416315322bf9a59
Gersi Doko <gersi.doko@unh.edu>
2024-02-29 19:50:16 -0500

    NixOS Rebuild: 91 current  2024-02-29 19:21:20  23.11.4575.526d051b128b  6.1.78                          *

--- rebuild_switch.sh
+++ rebuild_switch.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 pushd /etc/nixos
-set -e
 git --no-pager diff *.nix
 echo "NixOS Rebuilding Switch..."
-# sudo nixos-rebuild switch &>nixos-switch.log
-# (cat nixos-switch.log | grep --color error) && false
+sudo nixos-rebuild switch &>./nixos-switch.log
+(cat nixos-switch.log | grep --color error) && false
 echo "Pushing to git..."
 GEN=$(nixos-rebuild --fast list-generations | grep current)
 echo "Generation: $GEN"
